9cc10c565218ea4dc90bf1657c4d2d9fdc920d07
Code Nation 20 <codenation@CN-20.lan>
2019-02-21 16:45:28 +0000

    v0.1 - moveable ship, moving asteroids

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const ship_Size = 30; //ship height
 const turn_Speed = 360; // Turn Speed in deg per second
 const ship_thrust = 5 // acceleartion in pixels per second per second
 const friction = 0.7 // friction coefficient
-
+const roids_Num = 3 //starting asteroids
+const roids_Jag = 0.3 //jaggedness of asteroids 0 = none 1 = lots
+const roids_Speed = 50 // max speed
+const roids_Size = 100 // starting size of asteroids
+const roids_Vert = 10 //average sides (verts)
 
 /** @type {HTMLCanvasElement} */
 let canv = document.getElementById("gameCanvas")
@@ -23,10 +27,56 @@ let ship = {
     rot: 0,
     thrusting: false,
     thrust: {
-        x:0,
-        y:0
+        x: 0,
+        y: 0
     }
 }
+
+//  Asteroids object
+
+let roids = [];
+
+const newAsteroid = (x, y) => {
+    let roid = {
+        x: x,
+        y: y,
+        xv: Math.random() * roids_Speed / FPS * (Math.random() < 0.5 ? 1 : -1),
+        yv: Math.random() * roids_Speed / FPS * (Math.random() < 0.5 ? 1 : -1),
+        r: roids_Size / 2,
+        a: Math.random() * Math.PI * 2,
+        vert: Math.floor(Math.random() * (roids_Vert + 1) + roids_Vert / 2),
+        offs: []
+    }
+
+    //create offset array
+    for (let i = 0; i < roid.vert; i++){
+        roid.offs.push(Math.random() * roids_Jag * 2 + 1 - roids_Jag)
+    }
+    return roid
+}
+
+
+
+
+const distBetweenPoints = (x1, y1, x2, y2) => {
+    return Math.sqrt(Math.pow(x2 -x1, 2) + Math.pow(y2 - y1, 2))
+}
+
+const createAsteroidsBelt = () => {
+    roids = [];
+    let x, y;
+    for (let i = 0; i < roids_Num; i++) {
+        do{
+        x = Math.floor(Math.random() * canv.width)
+        y = Math.floor(Math.random() * canv.height)
+        } while (distBetweenPoints(ship.x, ship.y, x, y) < roids_Size * 2 + ship.r)
+        roids.push(newAsteroid(x, y))
+    }
+}
+
+
+
+createAsteroidsBelt()
 
 //  Update Game
 const update = () => {
@@ -35,14 +85,45 @@ const update = () => {
     ctx.fillRect(0, 0, canv.width, canv.height)
 
     //  Thrust Ship
-    if (ship.thrusting){
+    if (ship.thrusting) {
         ship.thrust.x += ship_thrust * Math.cos(ship.a) / FPS
         ship.thrust.y -= ship_thrust * Math.sin(ship.a) / FPS
+        // draw thruster
+        // ctx.fillStyle = "red"
+        ctx.strokeStyle = "orange"
+        ctx.lineWidth = ship_Size / 20
+        ctx.beginPath()
+        ctx.moveTo( //  rear left
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) + 0.7 * Math.sin(ship.a)),
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) - 0.7 * Math.cos(ship.a))
+        )
+        ctx.lineTo( //  Rear centre
+            ship.x - 4 / 3 * ship.r * Math.cos(ship.a),
+            ship.y + 4 / 3 * ship.r * Math.sin(ship.a)
+        )
+        ctx.lineTo( //Rear Right
+            ship.x - ship.r * (2 / 3 * Math.cos(ship.a) - 0.7 * Math.sin(ship.a)),
+            ship.y + ship.r * (2 / 3 * Math.sin(ship.a) + 0.7 * Math.cos(ship.a))
+        )
+        // ctx.closePath();
+        // ctx.fill()
+        ctx.stroke()
     } else {
         ship.thrust.x -= friction * ship.thrust.x / FPS
         ship.thrust.y -= friction * ship.thrust.y / FPS
     }
 
+    //  Edge of screen
+    if (ship.x < 0 - ship.r) {
+        ship.x = canv.width + ship.r
+    } else if (ship.x > canv.width + ship.r) {
+        ship.x = 0 - ship.r
+    }
+    if (ship.y < 0 - ship.r) {
+        ship.y = canv.height + ship.r
+    } else if (ship.y > canv.height + ship.r) {
+        ship.y = 0 - ship.r
+    }
 
     //  Draw Player
     ctx.strokeStyle = "white"
@@ -64,6 +145,60 @@ const update = () => {
     ctx.stroke()
 
 
+    //Draw Asteroids
+    ctx.strokeStyle = "white"
+    ctx.lineWidth = ship_Size / 10
+    let x, y, r, a, vert, offs;
+    for (let i = 0; i < roids.length; i++) {
+        //get asteroids properties
+        x = roids[i].x
+        y = roids[i].y
+        r = roids[i].r
+        a = roids[i].a
+        vert = roids[i].vert
+        offs = roids[i].offs
+        // draw a path
+        ctx.beginPath()
+        ctx.moveTo(
+            x + r * offs[0] * Math.cos(a),
+            y + r * offs[0] * Math.sin(a),
+        )
+
+        //draw polygon
+        for (let j = 1; j < vert; j++){
+            ctx.lineTo(
+                x + r * offs[j] * Math.cos( a + j * Math.PI * 2 / vert),
+                y + r * offs[j] * Math.sin( a + j * Math.PI * 2 / vert)
+            )
+        }
+        ctx.closePath()
+        ctx.stroke()
+
+        //move asteroid
+        roids[i].x += roids[i].xv
+        roids[i].y += roids[i].yv
+
+        //handle edge of screen
+        if (roids[i].x < 0 - roids[i].r){
+            roids[i].x = canv.width + roids[i].r
+        } else  if (roids[i].x > canv.width + roids[i].r){
+            roids[i].x = 0 - roids[i].r
+        }
+        if (roids[i].y < 0 - roids[i].r){
+            roids[i].y = canv.height + roids[i].r
+        } else  if (roids[i].y > canv.height + roids[i].r){
+            roids[i].y = 0 - roids[i].r
+        }
+        
+
+        
+
+
+
+        
+    }
+
+
     //  Rotate Player
     ship.a += ship.rot;
 
@@ -73,7 +208,7 @@ const update = () => {
     ship.y += ship.thrust.y
     //  Centre Dot
     ctx.fillStyle = "red"
-    ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2)
+    // ctx.fillRect(ship.x - 1, ship.y - 1, 2, 2)
 }
 
 
